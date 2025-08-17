bd499a62cafd1ac651185b7eacd196f534deeda8
sairambegari <sairambegari23@gmail.com>
2025-08-17 19:43:38 +0530

    app.js

--- js/app.js
+++ js/app.js
@@ -682,3 +682,20 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = "flex";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
